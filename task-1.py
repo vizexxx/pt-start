7cbc01704cef3578738767274f7ba53ebbbea5aa
Valeria Melnikova <150736925+vizexxx@users.noreply.github.com>
2025-03-27 16:21:19 +0300

    fix: task-1.py

--- task-1.py
+++ task-1.py
@@ -1,9 +1,10 @@
 import logging
 import re
 import paramiko
+import psycopg2
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ConversationHandler
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ConversationHandler, CallbackContext
 import pytz
 import os
 
@@ -12,6 +13,8 @@ load_dotenv('data.env')
 logging.basicConfig(
     filename='logfile.txt', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
+class CallbackContex:
+    pass
 
 logger = logging.getLogger(__name__)
 
@@ -26,28 +29,30 @@ async def start(update: Update, context):
 async def helpCommand(update: Update, context):
     await update.message.reply_text(
         "Доступные для использования команды:\n1. /start - приветствие;\n2. /find_email - выделение из текста email-адресов;\n"
-        "3. /find_phone_number - выделение из текста номеров телефонов;\n"
-        "4. /verify_password - проверка сложности пароля;\n"
-        "5. /get_release - информация о релизе системы;\n"
-        "6. /get_uname - информация об архитектуре процессора, имени хоста системы и версии ядра;\n"
-        "7. /get_uptime - информация о времени работы системы;\n"
-        "8. /get_df - информация о состоянии файловой системы;\n"
-        "9. /get_free - информация о состоянии оперативной памяти;\n"
-        "10. /get_mpstat - информация о производительности системы;\n"
-        "11. /get_w - информация о работающих в данной системе пользователях;\n"
-        "12. /get_auths - информация о последних 10 входах в систему;\n"
-        "13. /get_critical - информация о последних 5 критических событиях;\n"
-        "14. /get_ps - информация о запущенных процессах;\n"
-        "15. /get_ss - информация об используемых портах;\n"
-        "16. /get_apt_list - информация об установленных пакетах (при использовании можно ввести название пакета для поиска, /get_apt_list <имя_пакета>);\n"
-        "17. /get_services - информация о запущенных сервисах.\n")
-
+        "3. /get_emails - получение почтовых адресов из таблицы;\n"
+        "4. /find_phone_number - выделение из текста номеров телефонов;\n"
+        "5. /get_phone_numbers - получение телефонных номеров из таблицы;\n"
+        "6. /verify_password - проверка сложности пароля;\n"
+        "7. /get_release - информация о релизе системы;\n"
+        "8. /get_uname - информация об архитектуре процессора, имени хоста системы и версии ядра;\n"
+        "9. /get_uptime - информация о времени работы системы;\n"
+        "10. /get_df - информация о состоянии файловой системы;\n"
+        "11. /get_free - информация о состоянии оперативной памяти;\n"
+        "12. /get_mpstat - информация о производительности системы;\n"
+        "13. /get_w - информация о работающих в данной системе пользователях;\n"
+        "14. /get_auths - информация о последних 10 входах в систему;\n"
+        "15. /get_critical - информация о последних 5 критических событиях;\n"
+        "16. /get_ps - информация о запущенных процессах;\n"
+        "17. /get_ss - информация об используемых портах;\n"
+        "18. /get_apt_list - информация об установленных пакетах (при использовании можно ввести название пакета для поиска, /get_apt_list <имя_пакета>);\n"
+        "19. /get_services - информация о запущенных сервисах.\n"
+        "20. /get_repl_logs - получение файла с логами о репликации")
 
 async def findPhoneNumbersCommand(update: Update, context):
     await update.message.reply_text('Введите текст для поиска телефонных номеров: ')
     return FIND_PHONE_NUMBERS
 
-async def findPhoneNumbers(update: Update, context):
+async def findPhoneNumbers(update: Update, context: CallbackContex):
     user_input = update.message.text
     phoneNumRegex = re.compile(r'(?<!\d)(?:\+7|8)[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}(?!\d)')
     phoneNumberList = phoneNumRegex.findall(user_input)
@@ -59,9 +64,40 @@ async def findPhoneNumbers(update: Update, context):
     phoneNumbers = ''
     for i, number in enumerate(phoneNumberList, start=1):
         phoneNumbers += f'{i}. {number}\n'
-
     await update.message.reply_text(phoneNumbers)
-    return ConversationHandler.END
+    await update.message.reply_text('Вы хотите записать данные в таблицу phones? Формат ответа: yes/no.')
+    context.user_data['phoneNumberList'] = phoneNumberList
+    return writingTablePhones
+
+async def writingTablePhones(update: Update, context: CallbackContex):
+    content = update.message.text
+    if (content != 'yes' and content != 'no'):
+        await update.message.reply_text('Введите ответ в запрашиваемом типе: yes/no.')
+    else:
+        if (content == 'no'):
+            await update.message.reply_text('Телефонные номера не будут записаны')
+            return ConversationHandler.END
+        else:
+            phoneNumberList = context.user_data.get('phoneNumberList', [])
+            connection = psycopg2.connect(
+                host=os.getenv('DB_HOST'),
+                port=int(os.getenv('DB_PORT')),
+                user=os.getenv('DB_USER'),
+                password=os.getenv('DB_PASSWORD'),
+                database=os.getenv('DB_NAME')
+            )
+            db_connect = connection.cursor()
+            for phone in phoneNumberList:
+                try:
+                    db_connect.execute(
+                        f"INSERT INTO phones(phone_number) VALUES ('{phone}');"
+                    )
+                except psycopg2.Error as e:
+                    print(f"Ошибка при вставке {phone}: {e}")
+            connection.commit()
+            await update.message.reply_text('Телефонные номера записаны')
+            db_connect.close()
+            return ConversationHandler.END
 
 async def findEmailsCommand(update: Update, context):
     await update.message.reply_text('Введите текст для поиска email-адресов: ')
@@ -85,7 +121,113 @@ async def findEmails(update: Update, context):
         emails += f'{i}. {email}\n'
 
     await update.message.reply_text(emails)
-    return ConversationHandler.END
+    await update.message.reply_text('Вы хотите записать данные в таблицу emails? Формат ответа: yes/no.')
+    context.user_data['emailList'] = emailList
+    return writingTableEmails
+
+async def writingTableEmails(update: Update, context: CallbackContex):
+    content = update.message.text
+    if (content != 'yes' and content != 'no'):
+        await update.message.reply_text('Введите ответ в запрашиваемом типе: yes/no.')
+    else:
+        if (content == 'no'):
+            return ConversationHandler.END
+        else:
+            emailList = context.user_data.get('emailList', [])
+            connection = psycopg2.connect(
+            host=os.getenv('DB_HOST'),
+                port=int(os.getenv('DB_PORT')),
+                user=os.getenv('DB_USER'),
+                password=os.getenv('DB_PASSWORD'),
+                database=os.getenv('DB_NAME')
+            )
+            db_connect = connection.cursor()
+            for email in emailList:
+                try:
+                    db_connect.execute(
+                        f"INSERT INTO emails(email) VALUES ('{email}');"
+                    )
+                except psycopg2.Error as e:
+                    print(f"Ошибка при вставке {email}: {e}")
+            connection.commit()
+            await update.message.reply_text('Почтовые адреса записаны')
+            db_connect.close()
+            return ConversationHandler.END
+
+async def get_emails(update: Update, context):
+    connection = psycopg2.connect(
+        host=os.getenv('DB_HOST'),
+        port=int(os.getenv('DB_PORT')),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
+    )
+    db_connect = connection.cursor()
+    db_connect.execute(f"SELECT * FROM emails;")
+    results = db_connect.fetchall()
+    if not results:
+        return "В таблице emails нет записей"
+    result = "Почты в таблице emails:\n" + "\n".join(
+        f"{row[0]}. {row[1]}"
+        for i, row in enumerate(results)
+    )
+    await update.message.reply_text(result)
+    db_connect.close()
+
+async def get_phone_numbers(update: Update, context):
+    connection = psycopg2.connect(
+        host=os.getenv('DB_HOST'),
+        port=int(os.getenv('DB_PORT')),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
+    )
+    db_connect = connection.cursor()
+    db_connect.execute(f"SELECT * FROM phones;")
+    results = db_connect.fetchall()
+    if not results:
+        return "В таблице phones нет записей"
+    result = "Номера телефонов в таблице phones:\n" + "\n".join(
+        f"{row[0]}. {row[1]}"
+        for i, row in enumerate(results)
+    )
+    await update.message.reply_text(result)
+    db_connect.close()
+
+async def get_repl_logs(update: Update, context):
+    await update.message.reply_text(
+        "Создаю файл с логами...\nЭто может занять некоторое время."
+    )
+
+    result = execute_ssh_command("sudo tail /var/log/postgresql/postgresql-15-main.log > logs.txt")
+
+    if "Ошибка" in result:
+        await update.message.reply_text("Ошибка при создании файла с логами")
+        return
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(
+        os.getenv('RM_HOST'),
+        port=int(os.getenv('RM_PORT')),
+        username=os.getenv('RM_USER'),
+        password=os.getenv('RM_PASSWORD')
+    )
+    sftp = ssh.open_sftp()
+    remote_file = sftp.file('logs.txt', 'r')
+    local_file = 'logs.txt'
+    sftp.get('logs.txt', local_file)
+    sftp.close()
+    ssh.close()
+
+    await update.message.reply_document(
+        document=open(local_file, 'rb'),
+        caption="Файл с логами о репликации."
+    )
+
+    os.remove(local_file)
+    execute_ssh_command("rm logs.txt")
+
 
 async def echo(update: Update, context):
     await update.message.reply_text(update.message.text)
@@ -301,6 +443,7 @@ def main():
         entry_points=[CommandHandler("find_phone_number", findPhoneNumbersCommand)],
         states={
             FIND_PHONE_NUMBERS: [MessageHandler(filters.TEXT & ~filters.COMMAND, findPhoneNumbers)],
+            writingTablePhones: [MessageHandler(filters.TEXT & ~filters.COMMAND, writingTablePhones)],
         },
         fallbacks=[]
     )
@@ -309,6 +452,7 @@ def main():
         entry_points=[CommandHandler("find_email", findEmailsCommand)],
         states={
             FIND_EMAILS: [MessageHandler(filters.TEXT & ~filters.COMMAND, findEmails)],
+            writingTableEmails:[MessageHandler(filters.TEXT & ~filters.COMMAND, writingTableEmails)],
         },
         fallbacks=[]
     )
@@ -339,6 +483,10 @@ def main():
     application.add_handler(CommandHandler("get_ss", get_ss))
     application.add_handler(CommandHandler("get_apt_list", get_apt_list))
     application.add_handler(CommandHandler('get_services', get_services))
+    application.add_handler(CommandHandler('get_emails', get_emails))
+    application.add_handler(CommandHandler('get_phone_numbers', get_phone_numbers))
+    application.add_handler(CommandHandler('get_repl_logs', get_repl_logs))
+
 
     try:
         application.run_polling()
